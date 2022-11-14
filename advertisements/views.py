2d6b56218ef57e0c9fd4ce959a92d457a7bf52bb
Xypmich <kvudev@gmail.com>
2022-11-15 00:04:15 +0300

    fix permissions | add filter/order_by by status

--- advertisements/views.py
+++ advertisements/views.py
@@ -1,5 +1,4 @@
 from django_filters.rest_framework import DjangoFilterBackend
-from rest_framework.permissions import IsAuthenticated
 from rest_framework.throttling import UserRateThrottle, AnonRateThrottle
 from rest_framework.viewsets import ModelViewSet
 
@@ -11,17 +10,10 @@ from advertisements.serializers import AdvertisementSerializer
 
 class AdvertisementViewSet(ModelViewSet):
     """ViewSet для объявлений."""
-    queryset = Advertisement.objects.all()
+    queryset = Advertisement.objects.all().order_by('-status')
     serializer_class = AdvertisementSerializer
     throttle_classes = [UserRateThrottle, AnonRateThrottle]
     filter_backends = [DjangoFilterBackend]
     filterset_class = AdvertisementFilter
     permission_classes = (AdvDelete,)
-    # TODO: настройте ViewSet, укажите атрибуты для кверисета,
-    #   сериализаторов и фильтров
 
-    def get_permissions(self):
-        """Получение прав для действий."""
-        if self.action in ["create", "update", "partial_update"]:
-            return [IsAuthenticated()]
-        return []
